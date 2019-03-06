c796add1a1e0585f6220bb88f5aae07c377a9c7a
Jason Chu <jasonchu721@gmail.com>
2019-03-05 21:54:49 -0700

    basic stuff done

---
+++ calc_broken/index.js
@@ -0,0 +1,111 @@
+var oneBtn = document.getElementById("calc-one")
+var twoBtn = document.getElementById("calc-two")
+var threeBtn = document.getElementById("calc-three")
+var fourBtn = document.getElementById("calc-four")
+var fiveBtn = document.getElementById("calc-five")
+var sixBtn = document.getElementById("calc-six")
+var sevenBtn = document.getElementById("calc-seven")
+var eightBtn = document.getElementById("calc-eight")
+var nineBtn = document.getElementById("calc-nine")
+var zeroBtn = document.getElementById("calc-zero")
+
+var decimalBtn = document.getElementById("calc-decimal")
+var clearBtn = document.getElementById("calc-clear")
+var backspaceBtn = document.getElementById("calc-decimal")
+var displayValElement = document.getElementById("calc-display-val")
+
+var displayVal = "0"
+var pendingVal = "0"
+var evalStringArray = []
+
+var calcNumBtns = document.getElementsByClassName("calc-btn-num")
+var calcOperatorBtns = document.getElementsByClassName("calc-btn-operator")
+
+var updateDisplayVal = (clickObj) => {
+  var btnText = clickObj.target.innerText
+
+  if(displayVal === "0")
+    displayVal = ''
+
+    displayVal += btnText;
+    displayValElement.innerText = displayVal
+}
+
+var performOperation = function(clickObj) {
+  var operator =clickObj.target.innerText
+
+  switch (operator) {
+    case '+':
+      pendingVal = displayVal
+      displayVal = '0'
+      displayValElement.innerText = displayVal
+      evalStringArray.push(pendingVal)
+      evalStringArray.push('*')
+      break
+
+    case '-':
+    pendingVal = displayVal
+    displayVal = '0'
+    displayValElement.innerText = displayVal
+    evalStringArray.push(pendingVal)
+    evalStringArray.push('-')
+    break
+
+    case 'x':
+    pendingVal = displayVal
+    displayVal = '0'
+    displayValElement.innerText = displayVal
+    evalStringArray.push(pendingVal)
+    evalStringArray.push('*')
+    break
+
+    case '+':
+    pendingVal = displayVal
+    displayVal = '0'
+    displayValElement.innerText = displayVal
+    evalStringArray.push(pendingVal)
+    evalStringArray.push('/')
+    break
+
+    case '=':
+    evalStringArray.push(displayVal)
+    var evaluation = eval(evalStringArray.join(' '))
+    displayVal = evaluation + ' '
+    displayValElement.innerText = displayVal
+    evalStringArray = []
+    break
+  default:
+    break
+
+  }
+}
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+  calcNumBtns[i].addEventListener("click", updateDisplayVal, false)
+}
+
+for (let i = 0; i < calcOperationBtns.length; i++) {
+  calcOperationsBtns[i].addEventListener("click", performOperation, false)
+}
+
+clearBtn.addEventListener = ("click", function() {
+  displayVal = "0"
+  pendingVal = undefined;
+  evalStringArray = []
+  displayValElement.innerHTML = displayVal
+}
+// backspaceBtn.onlcick = () => {
+//   let lengthOfDisplayVal = displayVal.length
+//   displayVal = displayVal.slice(0, lengthOfDisplayVal - 1)
+
+//   if(displayVal === "")
+//     displayVal = "0"
+
+//   displayValElement.innerText = displayVal
+// }
+// decimalBtn.addEventListener = ("click", function() {
+//   if(displayVal.includes('.'))
+//     displayVal += '.'
+//   displayValElement.innerText = displayVal
+// }
+)
